a5b3d757aaeaf4e828330a91dfb25539c228588e
JiWonJeong06 <152679525+JiWonJeong06@users.noreply.github.com>
2025-09-09 20:33:53 +0900

    save

---
+++ Assets/Scripts/CharactorManager.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+// 테스트용
+// 나중에 JSON 파일 이용.
+public class CharactorManager : MonoBehaviour
+{
+    [Header("캐릭터 수치")]
+    public int damage; //공격력
+    public float maxhealth; //최대 체력
+    public float currenthealth; //현재 체력
+    public int defense; //방어력
+    public int speed; //이동 속도
+    public bool ranged; //원거리형인가?
+}
